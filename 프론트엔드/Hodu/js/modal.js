d548564fe493029e643ef34ca4927bdf12bcae7f
BlueHarmel <dldjdtjr@naver.com>
2024-01-18 11:27:25 +0900

    이메일 유효성 검사, css 수정

--- 프론트엔드/Hodu/js/modal.js
+++ 프론트엔드/Hodu/js/modal.js
@@ -1,13 +1,35 @@
 const modal = document.getElementById("modal");
 const openModalBtn = document.getElementById("open-modal");
 const closeModalBtn = document.getElementById("close-modal");
-// 모달창 열기
-openModalBtn.addEventListener("click", () => {
-    modal.style.display = "block";
-    document.body.style.overflow = "hidden"; // 스크롤바 제거
+let input = document.getElementById("email-check");
+
+// 이메일 조건 확인 후, 모달창 열기
+openModalBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    email = input.value;
+    if (emailCheck(email)) {
+        modal.style.display = "block";
+        document.body.style.overflow = "hidden"; // 스크롤바 제거
+    } else if (email === "") {
+        input.setCustomValidity("이메일이 입력되지 않았습니다.");
+        input.reportValidity(); // 유효성 메시지를 사용자에게 즉시 표시
+    } else {
+        input.setCustomValidity("이메일 형식이 잘못되었습니다.");
+        input.reportValidity(); // 유효성 메시지를 사용자에게 즉시 표시
+    }
 });
 // 모달창 닫기
 closeModalBtn.addEventListener("click", () => {
     modal.style.display = "none";
     document.body.style.overflow = "auto"; // 스크롤바 보이기
 });
+
+
+function emailCheck(email_address) {
+    email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+    if (!email_regex.test(email_address)) {
+        return false;
+    } else {
+        return true;
+    }
+}
