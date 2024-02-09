01a4b1d4514013e244ddb59947ac186c86f1b6f7
BlueHarmel <dldjdtjr@naver.com>
2024-02-10 05:59:51 +0900

    menu 입력 예외처리, AddressBook 변수명 수정

--- 백엔드/WeeklyQuiz/Second/Main.java
+++ 백엔드/WeeklyQuiz/Second/Main.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        AddressBook book = new AddressBook();
+        AddressBook addressBook = new AddressBook();
         Scanner sc = new Scanner(System.in);
         String name;
         String phoneNumber;
-        int choice = 0;
+        int choice;
         String menu = """
                 1. 비즈니스 연락처 추가
                 2. 개인 연락처 추가
@@ -18,10 +18,14 @@ public class Main {
                 6. 종료
                 """;
 
-        while(choice != 6) {
+        do {
             System.out.println(menu);
             System.out.print("원하는 옵션의 변호를 입력해주세요: ");
+            try{
             choice = Integer.parseInt(sc.nextLine());
+            } catch (NumberFormatException e){
+                choice = 8;
+            }
             switch (choice) {
                 case 1:
                     System.out.print("이름을 입력하세요: ");
@@ -31,7 +35,7 @@ public class Main {
                     System.out.print("회사명을 입력하세요: ");
                     String company = sc.nextLine();
                     BusinessContact busAddress = new BusinessContact(name, phoneNumber, company);
-                    book.contacts.add(busAddress);
+                    addressBook.contacts.add(busAddress);
                     break;
                 case 2:
                     PersonalContact perAddress = new PersonalContact();
@@ -41,27 +45,27 @@ public class Main {
                     perAddress.setPhoneNumber(sc.nextLine());
                     System.out.print("관계를 입력하세요: ");
                     perAddress.setRelation(sc.nextLine());
-                    book.contacts.add(perAddress);
+                    addressBook.contacts.add(perAddress);
                     break;
                 case 3:
-                    book.displayContacts();
+                    addressBook.displayContacts();
                     break;
                 case 4:
                     System.out.print("검색할 이름을 입력하세요: ");
-                    book.searchContact(sc.nextLine());
+                    addressBook.searchContact(sc.nextLine());
                     break;
                 case 5:
                     System.out.print("삭제할 사람의 이름을 입력하세요: ");
                     name = sc.nextLine();
                     System.out.print("삭제할 사람의 전화번호를 입력하세요: ");
                     phoneNumber = sc.nextLine();
-                    book.deleteContact(name,phoneNumber);
+                    addressBook.deleteContact(name,phoneNumber);
                     break;
                 case 6:
                     break;
                 default:
-                    System.out.println("유효하지 않은 옵션입니다. 다시 입력해주세요!");
+                    System.out.println("유효하지 않은 입력입니다. 다시 입력해주세요!");
             }
-        }
+        } while(choice != 6);
     }
 }
